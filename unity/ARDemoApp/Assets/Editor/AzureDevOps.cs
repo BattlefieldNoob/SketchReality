3278b331ff99e2ac0f78b4116fcc2920fb2eef98
Antonio <antonio.ruggiero@uqido.com>
2019-12-13 14:06:26 +0100

    updated unity, azure build file

---
+++ unity/ARDemoApp/Assets/Editor/AzureDevOps.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class AzureDevOps
+{
+
+    [MenuItem("Flutter/Android from Azure")]
+    public static void PerformAndroidBuild()
+    {
+        Build.DoBuildAndroidLibrary();
+        EditorApplication.Exit(0);
+    }
+}
