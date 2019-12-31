9b84c76f1ee3cf396bf4fe60086ad357a184c846
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2019-12-31 18:07:52 +0100

    fix assets not found

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,30 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<esys_flutter_share/EsysFlutterSharePlugin.h>)
 #import <esys_flutter_share/EsysFlutterSharePlugin.h>
+#else
+@import esys_flutter_share;
+#endif
+
+#if __has_include(<flutter_downloader/FlutterDownloaderPlugin.h>)
 #import <flutter_downloader/FlutterDownloaderPlugin.h>
+#else
+@import flutter_downloader;
+#endif
+
+#if __has_include(<flutter_unity_widget/FlutterUnityWidgetPlugin.h>)
 #import <flutter_unity_widget/FlutterUnityWidgetPlugin.h>
+#else
+@import flutter_unity_widget;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
 #import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
