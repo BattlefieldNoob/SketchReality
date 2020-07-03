7e510371757c050c3a684f1ab58fd00a3bf30a65
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2020-07-03 14:52:47 +0200

    removed android and ios, fix on model viewer web

--- android/app/src/main/kotlin/com/rexraphael/PolyAR/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.rexraphael.PolyAR
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/ui/screens/with_arkit_screen.dart
+++ lib/src/ui/screens/with_arkit_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
 
+import 'package:easy_web_view/easy_web_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget_example/src/sprintf.dart';
 import 'package:googleapis/poly/v1.dart';
-
 
 class WithARkitScreen extends StatefulWidget {
   @override
@@ -19,6 +19,14 @@ class CustomPopupMenu {
 class _WithARkitScreenState extends State<WithARkitScreen> {
 
 
+  final String html = '''
+  <!-- Import the component -->
+   <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+  <script nomodule src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"></script>
+  <!-- Use it like any other HTML element -->
+  <model-viewer style="height:100%;width:100%;input:focus{outline: none;};" src="{{src}}" alt="A 3D model of an astronaut" auto-rotate camera-controls></model-viewer>"
+  ''';
+
   Asset asset;
 
   @override
@@ -26,63 +34,37 @@ class _WithARkitScreenState extends State<WithARkitScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
+    asset = ModalRoute.of(context).settings.arguments as Asset;
+    asset.formats.forEach((element) {print(element.formatType);});
+    var modelUrl = asset.formats.firstWhere((element) => element.formatType.contains("GLTF2"))?.root?.url;
 
-    asset=ModalRoute.of(context).settings.arguments as Asset;
+    print(modelUrl);
+    if(modelUrl==null){
+      modelUrl="https://poly.googleapis.com/downloads/fp/1582670587239591/dtVRCEkNeK4/0xqBfdm5b3i/error%20pyramid.gltf";
+    }
+    var src=sprintf(html, [modelUrl]);
 
     debugPrint("argument:$asset");
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ARKIT Demo'),
-      ),
-      body:Text("MOMOMOM"));
-  }
-
-
-  void onUnityMessage(controller, message) {
-    print('Received message from unity: ${message.toString()}');
-  }
-
-  // Callback that connects the created controller to the unity controller
-  void onUnityCreated(controller) {
-    print("UNITY CREATED!");
-
-    /*var binfile=File();
-    binfile.url="BoxTextured0.bin";
-    binfile.relativePath="BoxTextured0.bin";
-
-    var texfile=File();
-    texfile.url="CesiumLogoFlat.png";
-    texfile.relativePath="CesiumLogoFlat.png";
-
-    var format=Format();
-    format.resources=List<File>();
-    format.resources.add(binfile);
-    format.resources.add(texfile);
-    format.formatType="GLTF_2";
-    format.formatComplexity=FormatComplexity();
-
-    var root=File();
-    root.url="BoxTextured.gltf";
-    root.relativePath="BoxTextured.gltf";
-
-    format.root=root;
-
-    
-    var formats=List<Format>();
-    formats.add(format);
-
-    var asset=Asset();
-    asset.name="BoxTextured.gltf";
-    asset.formats = formats;*/
-
-   /* var message=MessageHandler();
-    message.name="PolyAsset";
-    message.data=jsonEncode(this.asset);
-
-    _unityWidgetController.postMessageToManager(message);*/
+        appBar: AppBar(
+          title: Text('ARKIT Demo'),
+        ),
+        body: Container(
+            constraints: BoxConstraints.expand(),
+            child: EasyWebView(
+          src: src,
+          isHtml: true,
+          // Use Html syntax
+          isMarkdown: false,
+          // Use markdown syntax
+          convertToWidgets: false,
+          // Try to convert to flutter widgets
+          onLoaded: () {
+            print("Loaded!");
+          },
+        )));
   }
 }
